eb84fdfca1a4ab91b1b097bc1e235c039723c023
Oliver Rosner <Schilum23@Olivers-MacBook-Pro.local>
2014-08-24 14:49:02 +0200

    Initial Commit

---
+++ meinRabatt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  meinRabatt
+//
+//  Created by Oliver Rosner on 24.08.14.
+//  Copyright (c) 2014 Schilum23. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
